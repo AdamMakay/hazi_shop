22f97c0c9fb505ed2ca96be08f12a27bec0b4b1e
AdamMakay <makayadam1@gmail.com>
2026-02-25 13:48:59 +0100

    uzenet

--- 2026-02-25---web-ruh-z---kos-r-main/kod.js
+++ 2026-02-25---web-ruh-z---kos-r-main/kod.js
@@ -6,4 +6,9 @@ function visszaKep() {
 }
 
 function kivalaszt(melyikTermek) {
+    let db =prompt("Hany darabot szeretne: ")
+    let kosar = document.getElementById("kosar")
+    let kiiras = document.createElement("p")
+    kiiras.innerText=db + " " + melyikTermek
+    kosar.append(kiiras)
 }

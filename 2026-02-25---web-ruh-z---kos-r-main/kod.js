cae041abdf0353ef67d10e5595cd3e3184aa0b6f
makay.adam <makayadam1@gmail.com>
2026-02-27 10:56:49 +0100

    uzenet

--- 2026-02-25---web-ruh-z---kos-r-main/kod.js
+++ 2026-02-25---web-ruh-z---kos-r-main/kod.js
@@ -5,10 +5,19 @@ function nagyitKep(melyikKep) {
 function visszaKep() {
 }
 
-function kivalaszt(melyikTermek) {
-    let db =prompt("Hany darabot szeretne: ")
+function kivalaszt() {
+    //let db =prompt("Hany darabot szeretne: ")
+    let osszegMonitor = Number(document.getElementById('osszegMonitor').textContent)
+    let osszegRam = Number(document.getElementById('osszegRam').textContent)
+    let osszegHDD = Number(document.getElementById('osszegHDD').textContent)
+    let osszegSDD = Number(document.getElementById('osszegSSD').textContent)
+    let osszegTap = Number(document.getElementById('osszegTap').textContent)
     let kosar = document.getElementById("kosar")
-    let kiiras = document.createElement("p")
-    kiiras.innerText=db + " " + melyikTermek
-    kosar.append(kiiras)
+    let kiiras = document.getElementById("Osszeg")
+    
+    eredmeny = osszegMonitor + osszegRam + osszegSDD + osszegHDD + osszegTap
+    kiiras.innerHTML = "Összeg: " + eredmeny 
+    
+   
 }
+
